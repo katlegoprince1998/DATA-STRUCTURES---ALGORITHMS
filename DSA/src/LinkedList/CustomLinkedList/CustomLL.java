bc0f61a489db993f7e26abc064e8b07a43e3ed15
katlegoprince1998 <princekatlego357@gmail.com>
2024-02-24 07:04:06 +0200

    Added method to remove nodes and it values

--- DSA/src/LinkedList/CustomLinkedList/CustomLL.java
+++ DSA/src/LinkedList/CustomLinkedList/CustomLL.java
@@ -1,6 +1,6 @@
 package LinkedList.CustomLinkedList;
 
-public class CustomLL {
+public class CustomLL{
     /**
      * All LinkedList have a head and tail
      * I will also maintain the size of the LinkedList
@@ -14,7 +14,7 @@ public class CustomLL {
         this.size = 0;
     }
 
-    //add a node
+
     public void addFirst(int val){
         Node node = new Node(val); //Instantiate a new node object
         node.next = head; //Set the new node as a new head
@@ -23,6 +23,62 @@ public class CustomLL {
             tail = head; //if it is, set tail to point to the head
         }
     }
+
+    public void addLast(int val){
+        if(tail == null){
+            addFirst(val);
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void addAt(int val, int index){
+        if (index == 0){
+            addFirst(val);
+        }
+        if (index == size){
+            addLast(val);
+        }
+        Node temp = head;
+        for (int i = 1; i < index; i++){
+            temp = temp.next;
+        }
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+        size++;
+
+    }
+    public int removeFirst(){
+        int val = head.val;;
+        head = head.next;
+        head = head.next;
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public int removeLast(){
+        int val = tail.val;
+        Node temp = tail;
+        for (int i = 0; i < size - 1; i++){
+          temp = temp.next;
+        }
+        tail = temp;
+        tail.val = temp.val;
+        return val;
+    }
+    public int getCurrent(){
+        return head.val;
+    }
+    public int getNextValue(){
+        head = head.next;
+        return head.val;
+    }
+
 
     public void display(){
         Node temp = head;
@@ -33,11 +89,9 @@ public class CustomLL {
         System.out.println("END");
     }
 
-
     private class Node{
       private int val;
       private Node next;
-
 
       public Node(int val){
           this.val = val;
@@ -48,7 +102,6 @@ public class CustomLL {
       }
     }
 }
-
 /**
  * This class will demonstrate how linked list work internally
  * I aim to understand how the add, remove, insert work internally
--- DSA/src/LinkedList/CustomLinkedList/Main.java
+++ DSA/src/LinkedList/CustomLinkedList/Main.java
@@ -1,7 +1,7 @@
 package LinkedList.CustomLinkedList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CustomLL list = new CustomLL();
         list.addFirst(12);
         list.addFirst(13);
@@ -10,5 +10,18 @@ public class Main {
         list.addFirst(10);
         list.addFirst(9);
         list.display();
+        list.addLast(14);
+        list.display();
+        list.addAt(100, 2);
+        list.display();
+        System.out.println("The removed value is " + list.removeFirst());
+        list.display();
+        System.out.println("The current value is " + list.getCurrent());
+        System.out.println("The next value is " + list.getNextValue());
+        System.out.println("The last value removed is " + list.removeLast());
+        list.display();
     }
-}
+
+
+    }
+
