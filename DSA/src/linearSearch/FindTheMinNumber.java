73057c33c582297a351de1aa26b05792434d0887
Katlego Mashego <143069648+katlegoprince1998@users.noreply.github.com>
2024-03-01 17:12:03 +0200

    Update FindTheMinNumber.java

--- DSA/src/linearSearch/FindTheMinNumber.java
+++ DSA/src/linearSearch/FindTheMinNumber.java
@@ -2,11 +2,13 @@ package linearSearch;
 
 public class FindTheMinNumber {
     public static void main(String[] args) {
+        //Array
 
         int[] arr = {122, 255, 355, 45, 51, 16, 557, 558, 54559, 1510, 51211, 1512, 1513, 5451214};
         int answer = findTheMin(arr);
         System.out.println(answer);
     }
+    //Method to search
 
     private static int findTheMin(int[] arr) {
         if(arr.length == 0) {
