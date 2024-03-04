967370bb4c465349ea7d858a653da31f608e51bc
katlegoprince1998 <princekatlego357@gmail.com>
2024-03-05 01:19:48 +0200

    Circular Linked List

--- DSA/src/LinkedList/CustomLinkedList/CircllelyLinkedList/CCLinkedList.java
+++ DSA/src/LinkedList/CustomLinkedList/CircllelyLinkedList/CCLinkedList.java
@@ -9,10 +9,54 @@ public class CCLinkedList {
         head = null;
         tail = null;
     }
+     
+    public void add(int val){
+        Node node = new Node(val);
+        if(head == null){
+            tail = node;
+            head = node;
+            return;
+        }
 
-    public CCLinkedList(){
-
+        tail.next = node;
+        node.next = head; 
+        tail = node;
+        size++;
     }
+
+    public void delete(int val){
+        Node node = head;
+        if(node == null){
+            return;
+        }
+       if(node.value == val){
+         head = head.next;
+         tail.next = head;
+         return;
+       }
+
+       do{
+        Node currentNode = node.next;
+        if(currentNode.value == val){
+            node.next = currentNode.next;
+            break;
+        }
+       node = node.next;
+      }while(node != head);
+    }
+
+    public void display(){
+        Node node = head;
+        if(head != null){
+            do{
+                System.out.print(node.value + " -> ");
+                node = node.next;
+            }while(node != head);
+            System.out.println("HEAD");
+           
+        }
+    }
+ 
 
     private class Node{
         private int value;
--- DSA/src/LinkedList/CustomLinkedList/CircllelyLinkedList/Main.java
+++ DSA/src/LinkedList/CustomLinkedList/CircllelyLinkedList/Main.java
@@ -1,3 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        CCLinkedList list = new CCLinkedList();
+        list.add(12);
+        list.add(15);
+        list.add(18);
+        list.add(25);
+        list.add(1782);
+        list.display();
+        list.delete(15);
+        list.display();
+    }
 
 }
